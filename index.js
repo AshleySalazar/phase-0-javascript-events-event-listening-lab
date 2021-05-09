12df07d642a654f042b0e8fd7b3d99ae09672364
Ashley Salazar <ashleysalazar0902@gmail.com>
2021-05-09 09:27:19 -0500

    commit tree 157b744e53eee7818d2f08d19651e35f4c58896f

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-function addingEventListener() {
-}
+const addingEventListener = () => {
+    const input = document.getElementById('input');
+
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+    
+    input.addEventListener('click', clickAlert); 
+};
+
+
+
